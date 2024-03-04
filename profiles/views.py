50ca32649b77443c2220d58d073c5c36359e1a40
jillusc <jillus@hotmail.com>
2024-03-04 11:39:09 +0000

    Added JSON Web Token authentication

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 from django.db.models import Count
 from rest_framework import generics, filters
 from django_filters.rest_framework import DjangoFilterBackend
-from drf_api.permissions import IsOwnerOrReadOnly
+from profile_API.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
